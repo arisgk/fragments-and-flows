4b744919a1a33f40c3699368febb62e1eafae987
Aris Goudouras <aris.gk@gmail.com>
2025-10-08 00:12:26 +0300

    chore: update color theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,7 +18,7 @@ export default defineConfig({
         rehypeClassNames,
         {
           p: 'text-base/7 md:text-lg/9 text-left sm:text-justify mb-6',
-          a: 'text-rose-600 hover:text-rose-800 wrap-anywhere',
+          a: 'text-indigo-500 hover:text-indigo-700 wrap-anywhere',
           h1: 'text-3xl sm:text-4xl md:text-5xl font-serif font-bold mb-6',
           h2: 'text-xl sm:text-2xl md:text-3xl font-serif font-bold mb-6',
           h3: 'text-lg md:text-[1.375rem] font-serif font-bold mb-4',
